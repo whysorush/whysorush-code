143ec291babe4a25547f426fa1e4adc2f6eb42e7
Rushabh <rushabhshah5516@gmail.com>
2025-07-30 00:10:58 +0530

    Fix routing for GitHub Pages with correct basename

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.PROD ? '/whysorush-code' : '/'}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
